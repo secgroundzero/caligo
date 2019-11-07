bc7dbf093923e8112310bce8a781369f68d8ab8f
sconst <stella_const@hotmail.com>
2019-11-07 06:41:11 +0200

    changes setup server

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ sudo apt install npm
 sudo apt-get install sqlite3
 cd ../../Functions
 sqlite3 devices.db ".read DBCreation"
+chown www-data devices.db
+chmod +x 
 cd php
 touch port.txt
+chown www-data port.txt
 touch connectedDevices.txt
+chown www-data connectedDevices.txt
+cd ../..
+chown www-data Functions
+
