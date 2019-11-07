ede9f64f2285d094ae769d4aaf9fea0f0ce50d54
sconst <stella_const@hotmail.com>
2019-11-07 15:25:25 +0200

    client setup corrections

--- device/client.py
+++ device/client.py
@@ -4,7 +4,8 @@ import socket
 import random
 import os
 import subprocess
-
+import getpass
+import psutil
 #Identify Internal IP
 def getInternalIP():
 
@@ -24,40 +25,59 @@ def getExternalIP():
 def getRandomPort():
 	return random.randint(40000,65535)
 
+def checkPortFile():
+	if (os.path.isfile('port.txt')):
+		return True
+	else:
+		return False
+
 
 def main():
 	#Read Config File
 	f = open("Config", "r")
-	lines=f.readlines()
-	for l in lines:
-		if (l.startswith("SERVER_IP=")):
-			SERVER_IP=l[10:len(l)].strip().replace(" ","")
-		elif (l.startswith("SOCKET_PORT=")):
-			SOCKET_PORT=int(l[12:len(l)].strip().replace(" ",""))
-		elif(l.startswith("DEVICE_NAME=")):
-			DEVICE_NAME=l[12:len(l)].strip().replace(" ","")
-		elif(l.startswith("SSH_PORT_SERVER")):
-			SSH_PORT_SERVER=l[16:len(l)].strip().replace(" ", "")
+        lines=f.readlines()
+        for l in lines:
+        	if (l.startswith("SERVER_IP=")):
+                	SERVER_IP=l[10:len(l)].strip().replace(" ","")
+                elif (l.startswith("SOCKET_PORT=")):
+                	SOCKET_PORT=int(l[12:len(l)].strip().replace(" ",""))
+                elif(l.startswith("DEVICE_NAME=")):
+                	DEVICE_NAME=l[12:len(l)].strip().replace(" ","")
+                elif(l.startswith("SSH_PORT_SERVER")):
+                	SSH_PORT_SERVER=l[16:len(l)].strip().replace(" ", "")
 
+	if (checkPortFile()):
+		f=open("port.txt", "r")
+		lines=f.readlines()
+		SSHport=int(lines[0].strip().replace(" ",""))
+		process="ps -ef | grep -c "+ str(SSHport)
+		p=subprocess.check_output(process, shell=True)
+		if (int(p.strip())==1):
+			commandTo="autossh -M 0 root@"+str(SERVER_IP)+" -N -R "+str(SSHport)+":localhost:"+str(SSH_PORT_SERVER)
+		        subprocess.call(commandTo,shell=True)
+	else:
+		SSHport=getRandomPort()
+		f=open("port.txt", "w")
+		f.write(str(SSHport))
+		f.close()
+		internalIP=getInternalIP()
+		externalIP=getExternalIP()
+		username=getpass.getuser()
+		info="DEVICE_NAME:"+str(DEVICE_NAME)+"|SSH_PORT:"+str(SSHport)+"|EXTERNAL_IP:"+str(externalIP)+"|INTERNAL_IP:"+str(internalIP)+"|USER:"+str(username)
+		# Create a socket object
+		s=socket.socket()
+		#connect to the server
+		s.connect((SERVER_IP,SOCKET_PORT))
+		# send info
+		s.send(info)
+		#close socket
+		s.close()
+		print info
+			
+		commandTo="autossh -M 0 root@"+str(SERVER_IP)+" -N -R "+str(SSHport)+":localhost:"+str(SSH_PORT_SERVER)
+		print commandTo
+		subprocess.call(commandTo,shell=True)
 
-	internalIP=getInternalIP()
-	externalIP=getExternalIP()
-	SSHport=getRandomPort()
-	
-	info="DEVICE_NAME:"+str(DEVICE_NAME)+"|SSH_PORT:"+str(SSHport)+"|EXTERNAL_IP:"+str(externalIP)+"|INTERNAL_IP:"+str(internalIP)
-	# Create a socket object 
-	s = socket.socket()          	                	  
-	# connect to the server 
-	s.connect((SERVER_IP, SOCKET_PORT)) 
-	# send info
-	s.send(info)
-	# close socket
-	s.close()
-	print info
-	commandTo="ssh -N -R "+str(SSHport)+":localhost:"+str(SSH_PORT_SERVER) +" root@" + str(SERVER_IP)
-	#commandTo="autossh -M 0 root@"+ str(SERVER_IP) + " -p "+str(SSH_PORT_SERVER)+ " -N -R "+str(SSHport)+":localhost:"+str(SSH_PORT_SERVER)
-	print commandTo
-	subprocess.call(commandTo,shell=True)
 if __name__== "__main__":
 	main()
 
