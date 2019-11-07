faba6a622f1f3a486fb6222a80821686d6f7f2b7
sconst <stella_const@hotmail.com>
2019-11-07 06:09:29 +0200

    add git ignore - minor changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 sudo apt update
+cd webssh2/app
 sudo apt install nodejs
 sudo apt install npm
 sudo apt-get install sqlite3
-cd Functions
+cd ../../Functions
 sqlite3 devices.db ".read DBCreation"
 cd php
 touch port.txt
