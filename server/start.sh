53ccddeb55807bcb60ba949784c75bd2f34b2dfd
Yiannis Ioannides <yiannis@haze.local>
2020-01-08 16:25:35 +0200

    bug fixes
     Committer: secgroundzero <secgroundzero@gmail.com>

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 systemctl start apache2
-cp Config /var/www/html/caligo/
+cp Config /var/www/html/webserver/
 python server.py &
 python Functions/createPath.py
 cd webssh2/app
